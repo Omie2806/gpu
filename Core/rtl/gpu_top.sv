4a82279b3f4c646afe9eb5eed97f715ed7922577
Om <omgupta2806@gmail.com>
2026-06-05 14:51:29 +0530

    added jump and branching support and parameterized the gpu

--- Core/rtl/gpu_top.sv
+++ Core/rtl/gpu_top.sv
@@ -1,37 +1,63 @@
 module gpu_top (
     input logic clk,
     input logic reset,
-    output logic [15:0] debug_regs [0:3][0:15][0:15]
+    input logic [3:0] lane_select,
+    input logic[15 : 0] warp_mask [0 : 3], 
+    input logic[15 : 0] warp_pc [0 : 3],
+    output logic [15:0] debug_pc,     // preserves warp scheduler PC
+    output logic [15:0] debug_regs [0:3][0:15][0:15],  // exposes register file
+    output logic [15:0] debug_lw_out,    // exposes lw_out path  // preserves mem_req signal
+    output logic [15:0] debug_alu_result
 );
 
+parameter NUMBER_OF_WARPS   = 4;
+parameter ADDR_WIDTH        = 16;
+parameter DATA_WIDTH        = 16;
+parameter NUMBER_OF_THREADS = 16;
 
-parameter NUMBER_OF_WARPS = 4;
 logic mem_done;
-logic[1 : 0] warp_id_from_ms;
-logic[1 : 0] warp_id_to_ms;
-logic[1 : 0] current_warp_id;
-logic[15 : 0] warp_ready;
-logic[15 : 0] warp_ready_mask;
+logic[$clog2(NUMBER_OF_WARPS) - 1 : 0] warp_id_from_ms;
+logic[$clog2(NUMBER_OF_WARPS) - 1 : 0] warp_id_to_ms;
+logic[$clog2(NUMBER_OF_WARPS) - 1 : 0] current_warp_id;
+logic[ADDR_WIDTH - 1 : 0] warp_ready;
+logic[NUMBER_OF_THREADS - 1 : 0] warp_ready_mask;
+logic branch; //for branch instruction
+logic jump; //unconditional jump
 
 warp_scheduler #(
-    .NUMBER_OF_WARPS(NUMBER_OF_WARPS)
+    .NUMBER_OF_THREADS(NUMBER_OF_THREADS),
+    .NUMBER_OF_WARPS(NUMBER_OF_WARPS),
+    .ADDR_WIDTH(ADDR_WIDTH),
+    .DATA_WIDTH(DATA_WIDTH)
 ) warp_inst (
     .clk(clk),
     .reset(reset),
     .mem_req(mem_req),
     .mem_done(mem_done),
+    .zero(zero[0]), //for now no divergence (im using this for testing only will get all alu results as 0)
+    .less_than(less_than[0]), //same for now i assume no divergence
+    .branch(branch),
+    .jump(jump), //all active threads will jump no conditions 
+    .imm_out(imm_out),
     .halt(halt),
     .warp_id_from_ms(warp_id_from_ms),
+    .warp_pc(warp_pc),
+    .warp_mask(warp_mask),
     .warp_id_to_ms(warp_id_to_ms),
     .warp_ready(warp_ready),
     .warp_ready_mask(warp_ready_mask),
     .current_warp_id(current_warp_id)
 );
 
-
 //instr mem
 logic[31 : 0] instr;
-instr_mem instr_inst(
+instr_mem #(
+    .NUMBER_OF_THREADS(NUMBER_OF_THREADS),
+    .NUMBER_OF_WARPS(NUMBER_OF_WARPS),
+    .ADDR_WIDTH(ADDR_WIDTH),
+    .DATA_WIDTH(DATA_WIDTH)    
+) instr_inst (
+    .clk(clk),
     .pc(warp_ready),
     .instr(instr)
 );
@@ -49,32 +75,38 @@ assign A2 = instr[7 : 4];
 assign A3 = instr[11 : 8];
 
 //alu_source mux
-logic[15 : 0] alu_source_mux [0 : 15];
+logic[DATA_WIDTH - 1 : 0] alu_source_mux [0 : NUMBER_OF_THREADS - 1];
 
 //alu data path
-logic [15 : 0] RS1 [0 : 15];
-logic [15 : 0] RS2 [0 : 15];
-logic [15 : 0] alu_result[0 : 15];
+logic [DATA_WIDTH - 1 : 0] RS1 [0 : NUMBER_OF_THREADS - 1];
+logic [DATA_WIDTH - 1 : 0] RS2 [0 : NUMBER_OF_THREADS - 1];
+logic [DATA_WIDTH - 1 : 0] alu_result[0 : NUMBER_OF_THREADS - 1];
 // control path
 logic [3 : 0]  alu_control;
-logic zero [0 : 15];
+logic zero [0 : NUMBER_OF_THREADS - 1];
+logic less_than [0 : NUMBER_OF_THREADS - 1];
 
-logic[15 : 0] result_source_mux [0 : 15];
-logic[15 : 0] result_source_mux_warps [0 : NUMBER_OF_WARPS - 1][0 : 15];
+logic[DATA_WIDTH - 1 : 0] result_source_mux [0 : NUMBER_OF_THREADS - 1];
+logic[DATA_WIDTH - 1 : 0] result_source_mux_warps [0 : NUMBER_OF_WARPS - 1][0 : NUMBER_OF_THREADS - 1];
 logic result_source;
 
-logic [15 : 0] RS1_warps [0 : NUMBER_OF_WARPS - 1][0 : 15];
-logic [15 : 0] RS2_warps [0 : NUMBER_OF_WARPS - 1][0 : 15];
+logic [DATA_WIDTH - 1 : 0] RS1_warps [0 : NUMBER_OF_WARPS - 1][0 : NUMBER_OF_THREADS - 1];
+logic [DATA_WIDTH - 1 : 0] RS2_warps [0 : NUMBER_OF_WARPS - 1][0 : NUMBER_OF_THREADS - 1];
 
 logic[3 : 0] lw_destination_out;
 logic lw_ready;
-logic[1 : 0] lw_warp_id;
+logic[$clog2(NUMBER_OF_WARPS) - 1 : 0] lw_warp_id;
 genvar w,i;
 generate
 
     for (w = 0; w < NUMBER_OF_WARPS; w++) begin: warp_array
-        for(i = 0; i < 16; i++) begin: lane_array
-        reg_file reg_inst(
+        for(i = 0; i < NUMBER_OF_THREADS; i++) begin: lane_array
+        reg_file #(
+            .NUMBER_OF_THREADS(NUMBER_OF_THREADS),
+            .NUMBER_OF_WARPS(NUMBER_OF_WARPS),
+            .ADDR_WIDTH(ADDR_WIDTH),
+            .DATA_WIDTH(DATA_WIDTH)    
+        ) reg_inst(
             .clk(clk),
             .reset(reset),
             .A1(A1),
@@ -102,27 +134,28 @@ endgenerate
 
     genvar k;
     generate
-        for(k = 0; k < 16; k++)begin
+        for(k = 0; k < NUMBER_OF_THREADS; k++)begin
         alu alu_inst (
             .A(RS1[k]),
             .B(alu_source_mux[k]),
             .alu_result(alu_result[k]),
             .alu_control(alu_control),
-            .zero(zero[k])
+            .zero(zero[k]),
+            .less_than(less_than[k])
         );
         assign alu_source_mux[k]    = alu_source ? imm_out : RS2[k];
     end
     endgenerate
 
 always @(*) begin
-    for(integer k = 0; k < 16; k++) begin
+    for(integer k = 0; k < NUMBER_OF_THREADS; k++) begin
         RS1[k] = RS1_warps[current_warp_id][k];
         RS2[k] = RS2_warps[current_warp_id][k];
     end
 end
 
 always @(*) begin
-    for(integer w = 0; w < 4; w++)begin
+    for(integer w = 0; w < NUMBER_OF_WARPS; w++)begin
         for(integer i = 0; i < 16; i++)begin
             if(lw_ready) begin
             result_source_mux_warps[w][i] = lw_out[i];
@@ -134,12 +167,17 @@ always @(*) begin
 end
 
 //mem_scheduler
-logic[15 : 0] lw_out [0 : 15];
-logic[15 : 0] addr_out, lw_in, sw_out_mem;
+logic[DATA_WIDTH - 1 : 0] lw_out [0 : NUMBER_OF_THREADS - 1];
+logic[DATA_WIDTH - 1 : 0] addr_out, lw_in, sw_out_mem;
 logic stall;
 
 
-mem_scheduler scheduler_inst (
+mem_scheduler #(
+    .NUMBER_OF_THREADS(NUMBER_OF_THREADS),
+    .NUMBER_OF_WARPS(NUMBER_OF_WARPS),
+    .ADDR_WIDTH(ADDR_WIDTH),
+    .DATA_WIDTH(DATA_WIDTH)
+) scheduler_inst (
     //datapath
     .clk(clk),
     .reset(reset),
@@ -183,24 +221,24 @@ always @(*) begin
     reg_en = 0;
     // lsu_en = 0;
     // lw_or_sw = 0;
+    mem_req_raw = 0;
     alu_control = 4'b0000;
     halt        = 0;
-    mem_req_raw = 0;
     result_source = 0;
+    branch = 0;
+    jump = 0;
     case (opcode)
         4'b0000: begin //add
             alu_source = 0;
             alu_control = 4'b0000;
             reg_we = 1;
             reg_en = 1;
-
         end
         4'b0001: begin //sub
             alu_source = 0;
             alu_control = 4'b0001;
             reg_we = 1;
             reg_en = 1;
-
         end
         4'b0010: begin //mul
             alu_source = 0;
@@ -240,9 +278,38 @@ always @(*) begin
            reg_en = 1;
 //           mem_write = 1;
             mem_req_raw = 1;
-
         end
-        4'b1000: begin
+        4'b1000: begin //addi
+            alu_source = 1;
+            alu_control = 4'b0000;
+            reg_we = 1;
+            reg_en = 1;            
+        end
+        4'b1001: begin //subi
+            alu_source = 1;
+            alu_control = 4'b0001;
+            reg_we = 1;
+            reg_en = 1;
+        end
+        4'b1010: begin //beq
+            alu_source = 0;
+            alu_control = 4'b0001;
+            branch = 1;
+            reg_en = 1;
+        end
+        4'b1011: begin //blt
+            alu_source = 0;
+            alu_control = 4'b0001;
+            branch = 1;
+            reg_en = 1;
+        end
+        4'b1100: begin //jump
+            alu_source = 1;
+            alu_control = 4'b0000;
+            jump = 1;
+            reg_en = 1;
+        end
+        4'b1111: begin
             halt = 1;
         end
     endcase
@@ -253,8 +320,7 @@ logic mem_req_sent;
 
 always_ff @(posedge clk) begin
     if(reset) begin
-        mem_req_sent <= 0;
-        mem_req_raw  <= 0;
+        mem_req_sent  <= 0;
     end
     else begin
         if(mem_req_raw && !mem_req_sent) begin
@@ -291,6 +357,10 @@ data_mem data_mem_inst (
     .result(lw_in),
     .mem_write(mem_write)
 );
+
+assign debug_pc = warp_ready[lane_select];
+assign debug_lw_out = lw_out[lane_select];
+assign debug_alu_result = alu_result[lane_select];
 
     
 endmodule
