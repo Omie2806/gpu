1871c870c6fcc2d23d3319ae0596e4033a775313
Om <omgupta2806@gmail.com>
2026-06-07 02:00:14 +0530

    branch divergence support

--- Core/rtl/warp_scheduler.sv
+++ Core/rtl/warp_scheduler.sv
@@ -4,35 +4,44 @@ module warp_scheduler #(
     parameter DATA_WIDTH        = 16,
     parameter NUMBER_OF_THREADS = 16
 ) (
-    input logic         clk, 
-    input logic         reset,
-    input logic         branch,//tell if there is a branch instruction comes from the contrl unit
-    input logic         zero, //for beq and comes from the alu
-    input logic         less_than,
-    input logic         jump,
-    input logic[15 : 0] imm_out,//immediate to add with the pc for branch instruction 
-    input logic[NUMBER_OF_THREADS - 1 : 0] warp_mask [0 : NUMBER_OF_WARPS - 1], //initialize 
-    input logic[ADDR_WIDTH - 1 : 0] warp_pc [0 : NUMBER_OF_WARPS - 1],  //initialize
-    input logic         mem_req, //from top check from opcode
-    input logic         mem_done, //from mem_scheduler
-    input logic         halt, //from top
-    input logic[$clog2(NUMBER_OF_WARPS) - 1 : 0]  warp_id_from_ms, //to memory_scheduler
+    input logic                                    clk, 
+    input logic                                    reset,
+    input logic                                    branch_eq,//tell if there is a branch instruction comes from the contrl unit
+    input logic                                    branch_lt,
+    input logic[0 : NUMBER_OF_THREADS - 1]         zero, //for beq and comes from the alu
+    input logic[0 : NUMBER_OF_THREADS - 1]         less_than,
+    input logic                                    jump,
+    input logic[15 : 0]                            imm_out,//immediate to add with the pc for branch instruction 
+    input logic[15 : 0]                            reconverge_pc,
+    input logic[NUMBER_OF_THREADS - 1 : 0]         warp_mask [0 : NUMBER_OF_WARPS - 1], //initialize 
+    input logic[ADDR_WIDTH - 1 : 0]                warp_pc [0 : NUMBER_OF_WARPS - 1],  //initialize
+    input logic                                    mem_req, //from top check from opcode
+    input logic                                    mem_done, //from mem_scheduler
+    input logic                                    halt, //from top
+    input logic[$clog2(NUMBER_OF_WARPS) - 1 : 0]   warp_id_from_ms, //to memory_scheduler
 
     output logic[$clog2(NUMBER_OF_WARPS) - 1 : 0]  warp_id_to_ms, //from memory_scheduler
-    output logic[ADDR_WIDTH - 1 : 0] warp_ready, //pc of ready warp
-    output logic[NUMBER_OF_THREADS - 1 : 0] warp_ready_mask,
+    output logic[ADDR_WIDTH - 1 : 0]               warp_ready, //pc of ready warp
+    output logic[NUMBER_OF_THREADS - 1 : 0]        warp_ready_mask,
     // output logic         done
-    output logic[$clog2(NUMBER_OF_WARPS) - 1 : 0] current_warp_id
+    output logic[$clog2(NUMBER_OF_WARPS) - 1 : 0]  current_warp_id
 );
+
+localparam STACK_DEPTH = 8; //per warp
 
 reg[$clog2(NUMBER_OF_WARPS) - 1: 0] current_warp;
 
 //warp table
-reg[ADDR_WIDTH - 1 : 0] WARP_PC  [0 : NUMBER_OF_WARPS - 1];
-reg[NUMBER_OF_THREADS - 1 : 0] WARP_MASK[0 : NUMBER_OF_WARPS - 1];
+reg[ADDR_WIDTH - 1 : 0]        WARP_PC     [0 : NUMBER_OF_WARPS - 1][0 : STACK_DEPTH - 1];
+reg[ADDR_WIDTH - 1 : 0]        RECON_PC    [0 : NUMBER_OF_WARPS - 1][0 : STACK_DEPTH - 1];
+reg[NUMBER_OF_THREADS - 1 : 0] WARP_MASK   [0 : NUMBER_OF_WARPS - 1][0 : STACK_DEPTH - 1];
+reg                            BRANCH_VALID[0 : NUMBER_OF_WARPS - 1][0 : STACK_DEPTH - 1];
 
 reg WARP_STALL    [0 : NUMBER_OF_WARPS - 1];
 reg WARP_FINISHED [0 : NUMBER_OF_WARPS - 1];
+
+logic[$clog2(STACK_DEPTH) - 1 : 0] stack_pointer[NUMBER_OF_WARPS - 1 : 0];
+logic[$clog2(STACK_DEPTH) - 1 : 0] stack_pointer_internal;
 
 typedef enum logic [2 : 0] {
     IDLE            = 3'b000,
@@ -48,14 +57,24 @@ logic pc_en;
 always_ff @(posedge clk) begin
     if(reset) begin
         for(integer i = 0; i < NUMBER_OF_WARPS; i++) begin
-            WARP_STALL[i]    <= 0;
-            WARP_FINISHED[i] <= 0;
-            WARP_MASK[i]     <= warp_mask[i];  //will set mask and pcs when reset is active
-            WARP_PC[i]       <= warp_pc[i];
+            for(integer j = 1; j < STACK_DEPTH; j++) begin
+                WARP_STALL[i]       <= 0;
+                WARP_FINISHED[i]    <= 0;
+                WARP_MASK[i][j]     <= 0; //set mask and pc for stack > 0 as 0 in reset
+                WARP_PC[i][j]       <= 0;  
+                RECON_PC[i][j]      <= 0;
+                BRANCH_VALID[i][j]  <= 0;    
+            end
+            WARP_PC[i][0]      <= warp_pc[i];
+            WARP_MASK[i][0]    <= warp_mask[i];
+            RECON_PC[i][0]     <= 16'hFFFF;
+            BRANCH_VALID[i][0] <= 0;
+            stack_pointer[i]   <= 0;
         end
-        current_warp       <= 2'b00;
-        state_curr         <= IDLE;
-        pc_en              <= 0;
+        current_warp           <= 2'b00;
+        state_curr             <= IDLE;
+        pc_en                  <= 0;
+        stack_pointer_internal <= 0;
         //ill now have to extend each warps instruction storage size due to branch instructions 
         //also ill make each warp instr buffer size dynamic(receive it from the compiler)
         //and the mask too
@@ -70,6 +89,7 @@ always_ff @(posedge clk) begin
                 if(mem_req) begin
                     WARP_STALL[current_warp]   <= 1;
                     state_curr                 <= REQUESTING;
+                    WARP_PC[warp_id_from_ms][stack_pointer[current_warp]] <= WARP_PC[warp_id_from_ms][stack_pointer[current_warp]] + 1;
                 end
                 else if(mem_done) begin
                     WARP_STALL[warp_id_from_ms] <= 0;
@@ -78,12 +98,65 @@ always_ff @(posedge clk) begin
                     WARP_FINISHED[current_warp]    <= 1;
                     state_curr                     <= WARP_DONE;
                 end
-                else if((pc_en && branch && zero) || (pc_en && branch && less_than) || (pc_en && jump)) begin
-                    WARP_PC[current_warp] <= WARP_PC[current_warp] + imm_out;
+                else if(pc_en && branch_eq) begin
+                    if(BRANCH_VALID[current_warp][stack_pointer[current_warp] + 1] == 0) begin
+                        //taken branch
+                        WARP_PC[current_warp][stack_pointer[current_warp] + 1] <= WARP_PC[current_warp][stack_pointer[current_warp]] + imm_out;
+                        RECON_PC[current_warp][stack_pointer[current_warp] + 1] <= WARP_PC[current_warp][stack_pointer[current_warp]] + reconverge_pc;
+                        BRANCH_VALID[current_warp][stack_pointer[current_warp] + 1] <= 1; 
+                        WARP_PC[current_warp][stack_pointer[current_warp]] <= WARP_PC[current_warp][stack_pointer[current_warp]] + reconverge_pc + 1;                   
+                    end
+
+                    if(BRANCH_VALID[current_warp][stack_pointer[current_warp] + 2] == 0) begin
+                        //not taken branch
+                        WARP_PC[current_warp][stack_pointer[current_warp] + 2] <= WARP_PC[current_warp][stack_pointer[current_warp]] + 1;
+                        RECON_PC[current_warp][stack_pointer[current_warp] + 2] <= WARP_PC[current_warp][stack_pointer[current_warp]] + reconverge_pc;
+                        BRANCH_VALID[current_warp][stack_pointer[current_warp] + 2] <= 1;
+                    end
+                    for(integer i = 0; i < NUMBER_OF_THREADS; i++) begin
+                        WARP_MASK[current_warp][stack_pointer[current_warp] + 1][i] <= (zero[i] && (WARP_MASK[current_warp][stack_pointer[current_warp]][i]) == 1);
+                    end
+                    for(integer i = 0; i < NUMBER_OF_THREADS; i++) begin
+                        WARP_MASK[current_warp][stack_pointer[current_warp] + 2][i] <= (!zero[i] && (WARP_MASK[current_warp][stack_pointer[current_warp]][i]) == 1);
+                    end
+
+                    stack_pointer[current_warp] <= stack_pointer[current_warp] + 2;
+                end
+                else if(pc_en && branch_lt) begin
+                    if(BRANCH_VALID[current_warp][stack_pointer[current_warp] + 1] == 0) begin
+                        //taken branch
+                        WARP_PC[current_warp][stack_pointer[current_warp] + 1] <= WARP_PC[current_warp][stack_pointer[current_warp]] + imm_out;
+                        RECON_PC[current_warp][stack_pointer[current_warp] + 1] <= WARP_PC[current_warp][stack_pointer[current_warp]] + reconverge_pc;
+                        BRANCH_VALID[current_warp][stack_pointer[current_warp] + 1] <= 1; 
+                        WARP_PC[current_warp][stack_pointer[current_warp]] <= WARP_PC[current_warp][stack_pointer[current_warp]] + reconverge_pc + 1;                   
+                    end
+
+                    if(BRANCH_VALID[current_warp][stack_pointer[current_warp] + 2] == 0) begin
+                        //not taken branch
+                        WARP_PC[current_warp][stack_pointer[current_warp] + 2] <= WARP_PC[current_warp][stack_pointer[current_warp]] + 1;
+                        RECON_PC[current_warp][stack_pointer[current_warp] + 2] <= WARP_PC[current_warp][stack_pointer[current_warp]] + reconverge_pc;
+                        BRANCH_VALID[current_warp][stack_pointer[current_warp] + 2] <= 1;
+                    end
+
+                    for(integer i = 0; i < NUMBER_OF_THREADS; i++) begin
+                        WARP_MASK[current_warp][stack_pointer[current_warp] + 1][i] <= less_than[i];
+                    end
+                    for(integer i = 0; i < NUMBER_OF_THREADS; i++) begin
+                        WARP_MASK[current_warp][stack_pointer[current_warp] + 2][i] <= !less_than[i];
+                    end
+
+                    stack_pointer[current_warp] <= stack_pointer[current_warp] + 2;                    
+                end
+                else if (jump && pc_en) begin
+                    WARP_PC[current_warp][stack_pointer[current_warp]] <= WARP_PC[current_warp][stack_pointer[current_warp]] + imm_out;
+                end
+                else if(WARP_PC[current_warp][stack_pointer[current_warp]] == RECON_PC[current_warp][stack_pointer[current_warp]] && pc_en) begin
+                    BRANCH_VALID[current_warp][stack_pointer[current_warp]] <= 0;
+                    stack_pointer[current_warp] <= stack_pointer[current_warp] - 1;
                 end
                 else if(pc_en) begin
                     //update the pc
-                    WARP_PC[current_warp] <= WARP_PC[current_warp] + 1;
+                    WARP_PC[current_warp][stack_pointer[current_warp]] <= WARP_PC[current_warp][stack_pointer[current_warp]] + 1;                  
                 end
                 else begin
                     pc_en <= 1;
@@ -113,7 +186,7 @@ always_ff @(posedge clk) begin
                 end
                 if(mem_done) begin
                     WARP_STALL[warp_id_from_ms] <= 0;
-                    WARP_PC[warp_id_from_ms] <= WARP_PC[warp_id_from_ms] + 1;
+                    WARP_PC[warp_id_from_ms][stack_pointer[current_warp]] <= WARP_PC[warp_id_from_ms][stack_pointer[current_warp]] + 1;
                 end
             end
             default: state_curr <= IDLE;
@@ -123,6 +196,6 @@ end
 
     assign warp_id_to_ms   = current_warp;
     assign current_warp_id = current_warp;
-    assign warp_ready      = WARP_PC[current_warp];
-    assign warp_ready_mask = WARP_MASK[current_warp];
+    assign warp_ready      = WARP_PC[current_warp][stack_pointer[current_warp]];
+    assign warp_ready_mask = WARP_MASK[current_warp][stack_pointer[current_warp]];
 endmodule
