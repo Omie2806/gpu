53e0f6442e26685710ce07d016b0207e26a4fa71
Om <omgupta2806@gmail.com>
2026-06-07 01:59:12 +0530

    extended

--- Core/rtl/instr_mem.sv
+++ Core/rtl/instr_mem.sv
@@ -1,12 +1,19 @@
-module instr_mem (
-    input logic[15 : 0] pc,
-    output logic[31 : 0] instr
+module instr_mem#(
+    parameter NUMBER_OF_WARPS   = 4,
+    parameter ADDR_WIDTH        = 16,
+    parameter DATA_WIDTH        = 16,
+    parameter NUMBER_OF_THREADS = 16    
+) (
+    input logic clk,
+    input  logic [ADDR_WIDTH - 1 : 0] pc,
+    output logic [31:0] instr
 );
+reg [31:0] instr_mem [0:255];
 
-reg[31 : 0] instr_mem [0 : 255];
-//input program directly here or through the test bench
+// initial $readmemh("program.mem", instr_mem);
+
 always @(*) begin
-    instr = instr_mem[pc];
+    instr = instr_mem[pc[7:0]];
 end
 
 endmodule
