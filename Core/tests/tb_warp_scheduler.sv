0d129497d7475b638840f995433085430ce880e9
Om <omgupta2806@gmail.com>
2026-06-07 02:01:23 +0530

    new tests for branching

--- Core/tests/tb_warp_scheduler.sv
+++ Core/tests/tb_warp_scheduler.sv
@@ -1,231 +1,148 @@
 `timescale 1ns/1ps
 
-module tb_mem_system;
+module tb_warp_scheduler;
 
+    parameter NUMBER_OF_WARPS   = 4;
+    parameter ADDR_WIDTH        = 16;
+    parameter DATA_WIDTH        = 16;
+    parameter NUMBER_OF_THREADS = 16;
 
-logic clk;
-logic reset;
-logic [3:0] request;
-logic [15:0] active_mask;
+     logic                                    clk; 
+     logic                                    reset;
+     logic                                    branch_eq;//tell if there is a branch instruction comes from the contrl unit
+     logic                                    branch_lt;
+     logic                                    zero[0 : NUMBER_OF_THREADS - 1]; //for beq and comes from the alu
+     logic                                    less_than[0 : NUMBER_OF_THREADS - 1];
+     logic                                    jump;
+     logic[15 : 0]                            imm_out;//immediate to add with the pc for branch instruction 
+     logic[ADDR_WIDTH - 1 : 0]                reconverge_pc;
+     logic[NUMBER_OF_THREADS - 1 : 0]         warp_mask [0 : NUMBER_OF_WARPS - 1]; //initialize 
+     logic[ADDR_WIDTH - 1 : 0]                warp_pc [0 : NUMBER_OF_WARPS - 1];  //initialize
+     logic                                    mem_req; //from top check from opcode
+     logic                                    mem_done; //from mem_scheduler
+     logic                                    halt; //from top
+     logic[$clog2(NUMBER_OF_WARPS) - 1 : 0]   warp_id_from_ms; //to memory_scheduler
 
-logic [15:0] addr_in [0:15];
-logic [15:0] sw_out  [0:15];
-logic [15:0] lw_out  [0:15];
+     logic[$clog2(NUMBER_OF_WARPS) - 1 : 0]  warp_id_to_ms; //from memory_scheduler
+     logic[ADDR_WIDTH - 1 : 0]               warp_ready; //pc of ready warp
+     logic[NUMBER_OF_THREADS - 1 : 0]        warp_ready_mask;
+    //  logic         done
+     logic[$clog2(NUMBER_OF_WARPS) - 1 : 0]  current_warp_id;
 
-logic stall;
-logic mem_write;
-
-logic [15:0] addr_out;
-logic [15:0] sw_out_mem;
-logic [15:0] lw_in;
-
-logic mem_req;
-logic mem_done;
-logic[1 : 0] warp_id_from_ws;
-logic[1 : 0] warp_id_to_ws;
-
-//warp scheduler
-logic halt;
-logic[1 : 0] warp_id_to_ms;
-
-logic[1 : 0] warp_id_from_ms;
-// logic done;
-logic[15 : 0] warp_ready;
-logic[15 : 0] warp_ready_mask;
+warp_scheduler #(
+    .NUMBER_OF_THREADS(NUMBER_OF_THREADS),
+    .NUMBER_OF_WARPS(NUMBER_OF_WARPS),
+    .ADDR_WIDTH(ADDR_WIDTH),
+    .DATA_WIDTH(DATA_WIDTH)
+) dut(
+    .*
+);
 
 initial begin
     clk = 0;
     forever #5 clk = ~clk;
 end
 
-mem_scheduler ms_dut(
-    .clk(clk),
-    .reset(reset),
-    .request(request), //standalone
-    .active_mask(warp_ready_mask),
-    .addr_in(addr_in), //standalone
-    .sw_out(sw_out), //standalone
-    .lw_out(lw_out), 
-    .stall(stall),
-    .mem_write(mem_write), 
-    .lw_in(lw_in), //standalone
-    .addr_out(addr_out),
-    .sw_out_mem(sw_out_mem),
-
-    //warp scheduler 
-    .mem_req(mem_req), //standalone
-    .mem_done(mem_done),
-    .warp_id_from_ws(warp_id_to_ms),
-    .warp_id_to_ws(warp_id_from_ms)
-);
-
-warp_scheduler ws_dut(
-    .clk(clk),
-    .reset(reset),
-    .mem_req(mem_req), //standalone
-    .mem_done(mem_done),
-    .halt(halt), //standalone
-    .warp_ready(warp_ready),
-    .warp_ready_mask(warp_ready_mask),
-    .warp_id_from_ms(warp_id_from_ms),
-    .warp_id_to_ms(warp_id_to_ms)
-);
-
 initial begin
-    $dumpfile("gpu_mem.vcd");
-    $dumpvars(0,tb_mem_system);
 
-    reset   = 1;
-    request = 4'b0000;
-    lw_in   = 16'h0000;
-    mem_req = 0;
-    halt    = 0;
-    for(int i = 0; i < 16; i++) begin
-        addr_in[i] = 16'h0100 + i; 
-        sw_out[i]  = 16'h00FF + i;  
-    end
-    repeat(4)@(posedge clk);
-    reset = 0;
+    $dumpfile("warp_scheduler.vcd");
+    $dumpvars(0, tb_warp_scheduler);
 
-    //test 1 - execute warp 0 as sw
-    repeat(2)@(posedge clk);
-    mem_req = 1;
-    request = 4'b0111;
-    @(posedge clk);
-    mem_req = 0;
-    request = 4'b0000;
-    repeat(49)@(posedge clk);
-    halt = 1;
-
-    //finish execute warp 3
-    @(posedge clk);
-    halt = 0;
-    repeat(16)@(posedge clk);
-    halt = 1;
-
-        //finish execute warp 0
-    @(posedge clk);
-    halt = 0;
-    repeat(16)@(posedge clk);
-    halt = 1;
-
-        //finish execute warp 4
-    @(posedge clk);
-    halt = 0;
-    repeat(20)@(posedge clk);
-    halt = 1;
-
-    //test 2
-    reset   = 1;
-    request = 4'b0000;
-    lw_in   = 16'h0000;
-    mem_req = 0;
-    halt    = 0;
-    for(int i = 0; i < 16; i++) begin
-        addr_in[i] = 16'h0100 + i; 
-        sw_out[i]  = 16'h00FF + i;  
-    end
-    repeat(4)@(posedge clk);
-    reset = 0;
-
-    //test 2 warp 0 and warp 1 have sw
-    @(posedge clk);
-    mem_req = 1;
-    request = 4'b0111;
-    @(posedge clk);
-    mem_req = 0;
-    request = 4'b0000;
-
-    //warp 1 has sw
-    @(posedge clk);
-    mem_req = 1;
-    request = 4'b0111;
-    @(posedge clk);
-    mem_req = 0;
-    request = 4'b0000;
-
-    //let them complete while warp 2 runs
-    repeat(100)@(posedge clk);
-    halt = 1; //warp 2 finsih
-    @(posedge clk);
-    halt = 0; //warp 0 resums
-    repeat(17)@(posedge clk);
-    //warp 0 ends
-    halt = 1;
-
-    @(posedge clk);
-    halt = 0; //warp 1 resumes
-    repeat(17)@(posedge clk);
-    halt = 1; //warp 1 ends
-
-    @(posedge clk);
-    halt = 0;//warp 3 resumes
-    repeat(17)@(posedge clk);
-    halt = 1;//warp 3 ends
+    reset    = 1;
+    mem_req  = 0;
+    mem_done = 0;
+    halt     = 0;
+    
+    warp_pc[0] = 16'h0000;
+    warp_mask[0] = 16'hFFFF;
+    warp_pc[1] = 16'h0032;
+    warp_mask[1] = 16'hFFFF;    
+    warp_pc[2] = 16'h0064;
+    warp_mask[2] = 16'hFFFF;    
+    warp_pc[3] = 16'h0096;
+    warp_mask[3] = 16'hFFFF;    
 
     repeat(3)@(posedge clk);
-
-    reset   = 1;
-    request = 4'b0000;
-    lw_in   = 16'h0000;
-    mem_req = 0;
-    halt    = 0;
-    for(int i = 0; i < 16; i++) begin
-        addr_in[i] = 16'h0100 + i; 
-        sw_out[i]  = 16'h00FF + i;  
-    end
-    repeat(4)@(posedge clk);
     reset = 0;
 
-    //test 4 all warps stall
-    @(posedge clk); //warp 0 stall
-    mem_req = 1;
-    request = 4'b0111;
     @(posedge clk);
-    mem_req = 0;
-    request = 4'b0000;
+    branch_eq = 1;
+    zero[0] = 1;
+    zero[1] = 1; 
+    zero[2] = 1;
+    zero[3] = 1;
+    zero[4] = 1;
+    zero[5] = 1;
+    zero[6] = 1;
+    zero[7] = 1;
 
-    repeat(2)@(posedge clk); //warp 2 stall
-    mem_req = 1;
-    request = 4'b0111;
+    zero[8] = 1;
+    zero[9] = 1;
+    zero[10] = 0;
+    zero[11] = 0;
+    zero[12] = 0;
+    zero[13] = 0;
+    zero[14] = 0;
+    zero[15] = 0; 
+    imm_out = 16'h0000_0008;
+    reconverge_pc = 16'h0000_000F;
+    @(posedge clk); 
+    branch_eq = 0;
+    repeat(3)@(posedge clk); 
+
     @(posedge clk);
-    mem_req = 0;
-    request = 4'b0000;
+    branch_eq = 1;
+    zero[0] = 1;
+    zero[1] = 1; 
+    zero[2] = 1;
+    zero[3] = 1;
+    zero[4] = 1;
+    zero[5] = 1;
+    zero[6] = 1;
+    zero[7] = 1;
 
-    @(posedge clk); //warp 2 stall
-    mem_req = 1;
-    request = 4'b0111;
+    zero[8] = 0;
+    zero[9] = 0;
+    zero[10] = 0;
+    zero[11] = 0;
+    zero[12] = 0;
+    zero[13] = 0;
+    zero[14] = 0;
+    zero[15] = 0; 
+    imm_out = 16'h0000_0008;
+    reconverge_pc = 16'h0000_000F;
+    @(posedge clk); 
+    branch_eq = 0;
+    repeat(50)@(posedge clk);     
+
     @(posedge clk);
-    mem_req = 0;
-    request = 4'b0000;
+    branch_lt = 1;
+    less_than[0] = 1;
+    less_than[1] = 1; 
+    less_than[2] = 1;
+    less_than[3] = 1;
+    less_than[4] = 0;
+    less_than[5] = 0;
+    less_than[6] = 0;
+    less_than[7] = 0;
 
-    repeat(5)@(posedge clk); //warp 3 stall
-    mem_req = 1;
-    request = 4'b0111;
-    @(posedge clk);
-    mem_req = 0;
-    request = 4'b0000;
-
-    repeat(64)@(posedge clk);
-    halt = 1; //warp 0 finishes
-    @(posedge clk);
-
-    halt = 0; //warp 1 continues
-    repeat(48)@(posedge clk);
-    halt = 1; //warp 1 finish
-    @(posedge clk);
-
-    halt = 0; //warp 2 continues
-    repeat(48)@(posedge clk);
-    halt = 1; //warp 2 finish
-    @(posedge clk);
-
-    halt = 0; //warp 3 continues
-    repeat(48)@(posedge clk);
-    halt = 1; //warp 3 finish
-    @(posedge clk);
-
+    less_than[8] = 0;
+    less_than[9] = 0;
+    less_than[10] = 0;
+    less_than[11] = 0;
+    less_than[12] = 0;
+    less_than[13] = 0;
+    less_than[14] = 0;
+    less_than[15] = 0; 
+    imm_out = 16'h0000_0004;
+    reconverge_pc = 16'h0000_000B;
+    @(posedge clk); 
+    branch_lt = 0;   
+    repeat(50)@(posedge clk); 
 
     $finish;
+
+    
 end
+
 endmodule
